61d71fc77d54e01d54476e5dd499d2dea582d22a
Rash AI <RashAI@dtek.com>
2023-10-10 19:19:21 +0300

    init

---
+++ src/js/task-03.js
@@ -0,0 +1,25 @@
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const ingredRef = document.querySelector("ul.gallery");
+
+const listElem = images
+  .map(
+    ({ url, alt }) => `<li><img class="image" src="${url}" width="160";
+    height="120" alt="${alt}"> </li>`
+  )
+  .join(" ");
+
+ingredRef.insertAdjacentHTML("beforeend", listElem);
